3012da9cd9db7f23b3d3a7bb752fd32250f57544
Janne Koponen <janne.koponen@wunder.io>
2016-10-31 13:12:10 +0200

    Add lpass questimate password.

--- .zsh/lpass_shortcuts.zsh
+++ .zsh/lpass_shortcuts.zsh
@@ -4,8 +4,8 @@ function lpass_shortcuts()
   git_clear_vars
 
   if [[ "$#" -eq 1 ]]; then
-    results=(${(f)"$(lpass ls | grep -i $1)"} )
-    results_color=(${(f)"$(lpass ls | grep -i --color=always $1)"} )
+    results=(${(f)"$(lpass ls | grep -i "$1")"} )
+    results_color=(${(f)"$(lpass ls | grep -i --color=always "$1")"} )
   fi
   i=1
   for line in $results_color
@@ -23,6 +23,24 @@ function lpass_shortcuts()
   done
 }
 
+function questimate_password()
+{
+  results=(${(f)"$(lpass ls | grep -i "$1")"} )
+
+  for line in $results
+  do
+    note_id="${${line#*id: }%]*}"
+    break
+  done
+
+  if [ $# -eq 1 ]; then
+    lpass show $note_id -c --field="www-admin"
+  elif [ $# -eq 2 ]; then
+    lpass show $note_id -c --field="$2"
+  fi
+}
+
 alias fp="lpass_shortcuts"
 alias lp="exec_scmb_expand_args lpwd"
 alias lps="exec_scmb_expand_args lpass show"
+alias qp=questimate_password
